0251f130d55bc309a02ff55e52510637050bebe5
alptkn <alptknheat@gmail.com>
2019-12-19 19:50:07 +0300

    Add files via upload

---
+++ dic_hepsiburada.py
@@ -0,0 +1,28 @@
+https://www.hepsiburada.com/lenovo/laptop-notebook-dizustu-bilgisayarlar-c-98
+https://www.hepsiburada.com/hp/laptop-notebook-dizustu-bilgisayarlar-c-98
+https://www.hepsiburada.com/dell-asus-lenovo/laptop-notebook-dizustu-bilgisayarlar-c-98
+
+https://www.hepsiburada.com/dell-lenovo/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=islemcitipi:Intel%E2%82%AC20Core%E2%82%AC20i5,Intel%E2%82%AC20Core%E2%82%AC20i7
+https://www.hepsiburada.com/dell-lenovo/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=islemcitipi:Intel%E2%82%AC20Core%E2%82%AC20i5,Intel%E2%82%AC20Core%E2%82%AC20i7;VariantList.Ram%E2%82%AC20%E2%82%AC28Sistem%E2%82%AC20Belle%E2%82%ACC4%E2%82%AC9Fi%E2%82%AC29:4%E2%82%AC20GB
+
+https://www.hepsiburada.com/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=islemcitipi:Intel%E2%82%AC20Core%E2%82%AC20i7
+https://www.hepsiburada.com/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=islemcitipi:Intel%E2%82%AC20Core%E2%82%AC20i5
+https://www.hepsiburada.com/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=islemcitipi:Intel%E2%82%AC20Core%E2%82%AC20i3
+https://www.hepsiburada.com/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=islemcitipi:AMD%E2%82%AC20Ryzen%E2%82%AC205
+https://www.hepsiburada.com/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=islemcitipi:Intel%E2%82%AC20Core%E2%82%AC20i3,Intel%E2%82%AC20Core%E2%82%AC20i7
+
+
+https://www.hepsiburada.com/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=VariantList.Ram%E2%82%AC20%E2%82%AC28Sistem%E2%82%AC20Belle%E2%82%ACC4%E2%82%AC9Fi%E2%82%AC29:4%E2%82%AC20GB
+https://www.hepsiburada.com/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=VariantList.Ram%E2%82%AC20%E2%82%AC28Sistem%E2%82%AC20Belle%E2%82%ACC4%E2%82%AC9Fi%E2%82%AC29:8%E2%82%AC20GB
+https://www.hepsiburada.com/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=VariantList.Ram%E2%82%AC20%E2%82%AC28Sistem%E2%82%AC20Belle%E2%82%ACC4%E2%82%AC9Fi%E2%82%AC29:16%E2%82%AC20B
+https://www.hepsiburada.com/laptop-notebook-dizustu-bilgisayarlar-c-98?filtreler=VariantList.Ram%E2%82%AC20%E2%82%AC28Sistem%E2%82%AC20Belle%E2%82%ACC4%E2%82%AC9Fi%E2%82%AC29:16%E2%82%AC20GB,8%E2%82%AC20GB
+
+
+CPU = {"intel i7" : "?filtreler=islemcitipi:Intel%E2%82%AC20Core%E2%82%AC20i7",
+	   "intel i5" : "?filtreler=islemcitipi:Intel%E2%82%AC20Core%E2%82%AC20i5",
+	   "intel i3" : "?filtreler=islemcitipi:AMD%E2%82%AC20Ryzen%E2%82%AC205"}
+
+
+Ram = {"4gb"  : "filtreler=VariantList.Ram%E2%82%AC20%E2%82%AC28Sistem%E2%82%AC20Belle%E2%82%ACC4%E2%82%AC9Fi%E2%82%AC29:4%E2%82%AC20GB",
+	   "8gb"  : "?filtreler=VariantList.Ram%E2%82%AC20%E2%82%AC28Sistem%E2%82%AC20Belle%E2%82%ACC4%E2%82%AC9Fi%E2%82%AC29:8%E2%82%AC20GB",
+	   "16gb" : "?filtreler=VariantList.Ram%E2%82%AC20%E2%82%AC28Sistem%E2%82%AC20Belle%E2%82%ACC4%E2%82%AC9Fi%E2%82%AC29:16%E2%82%AC20B"}
